fdda221027bcfbfa59a489c2c6d124a6a35221f8
kevinconly <kevinsezerano2@gmail.com>
2026-04-13 12:07:26 +0200

    Adding Authentication (login and sign up)

--- backend-application/app.js
+++ backend-application/app.js
@@ -7,10 +7,12 @@ const conn = require("./db/connection");
 
 //IMPORT ROUTES
 const User = require("./routes/userRoute");
+const auth = require("./routes/Auth")
 
 app.use(express.json());
 app.use(cors());
 app.use("/users", User);
+app.use("/auth", auth)
 
 app.listen(PORT, console.log(`app is live at http://localhost:3500`));
 
---
+++ backend-application/routes/Auth.js
@@ -0,0 +1,68 @@
+require('dotenv').config()
+const mysql = require("mysql2");
+const conn = require("../db/connection");
+const bcrypt = require("bcrypt");
+const express = require("express");
+const route = express.Router();
+const jwt = require('jsonwebtoken')
+const SECRETE = process.env.SECRETE
+
+//  SING UP
+route.post("/signup", (req, res) => {
+  const getUserEmailFromDb = "SELECT user_email FROM users WHERE user_email=?";
+  const { user_email, user_name, user_password } = req.body;
+  conn.query(getUserEmailFromDb, [user_email], async (err, result) => {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        message: `Failed to get user with this email: '${user_email}'`,
+        err,
+      });
+    } else if (result.length > 0) {
+      res.status(200).json({
+        message: `This Email is already signed up, login instead`,
+        result,
+      });
+    } else {
+      const hashedPassword = await bcrypt.hash(user_password, 12);
+      const insertUserIntoDatabase =
+        "INSERT INTO users (user_email, user_name, user_password, created_date) VALUES (?,?,?,?)";
+      const date_created = new Date();
+      conn.query(
+        insertUserIntoDatabase,
+        [user_email, user_name, hashedPassword, date_created],
+        (err, result) => {
+          if (err) {
+            res.status(500).json({ message: `Failed to add User`, err });
+          } else {
+            res.status(201).json({ message: `User registred successfully` });
+          }
+        },
+      );
+    }
+  });
+});
+
+//LOGIN
+
+route.post('/login', (req, res)=>{
+    const {user_email, user_password} = req.body
+    const check_email_in_db = 'SELECT * FROM users WHERE user_email = ?'
+    conn.query(check_email_in_db, [user_email], async(err, result)=>{
+        if(err){
+            res.status(500).json({message:'Failed to find Your Email'})
+        }else if(result.length <0){
+            res.status(403).json({message:"Email not registred yet"})
+        }else{
+            const user = result[0]
+            const is_password_match = await bcrypt.compare(user_password, user.user_password)
+            if(is_password_match === false){
+                res.status(401).json({message:"Wrong password"})
+            }else{
+                const token = jwt.sign( user.user_id, SECRETE, {expiresIn:"1d"})
+            }
+        }
+    })
+})
+
+module.exports =route
